239b287f67ff7eae6aabc031e2fd30618810b8df
Mel-Rocha <rochamel73@gmail.com>
2024-03-20 20:23:22 -0300

    more generic views

--- polls/apiviews.py
+++ polls/apiviews.py
@@ -4,12 +4,21 @@ from polls.models import Poll, Choice
 from polls.serializers import PollSerializer, ChoiceSerializer, \
     VoteSerializer
 
+
 class PollList(generics.ListCreateAPIView):
     queryset = Poll.objects.all()
     serializer_class = PollSerializer
 
 
-
 class PollDetail(generics.ListCreateAPIView):
     queryset = Poll.objects.all()
     serializer_class = PollSerializer
+
+
+class ChoiceList(generics.ListCreateAPIView):
+    queryset = Choice.objects.all()
+    serializer_class = ChoiceSerializer
+
+
+class CreateVote(generics.CreateAPIView):
+    serializer_class = VoteSerializer
